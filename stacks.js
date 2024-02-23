06c01b183d7eae3482770385106e63e812a95af8
Liz Robson <lizziearobson@gmail.com>
2024-02-23 18:43:00 +0000

    adding sets

--- stacks.js
+++ stacks.js
@@ -1,6 +1,6 @@
 let letters = [];
 
-let word = "racecar";
+let word = "liz";
 
 let rword = "";
 
@@ -8,8 +8,8 @@ for (let i = 0; i < word.length; i++) {
   letters.push(word[i]);
 }
 
-for (let i = 0; i < word.length; i++) {
-  rword += letters.pop();
+for (let i = 0; i < letters.length; i++) {
+  rword = rword + letters.pop();
 }
 
 if (rword === word) {
