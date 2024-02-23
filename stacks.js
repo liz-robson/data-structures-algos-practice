45b3765c60c3418f7465fdf8bb89a74a13e8361f
Liz Robson <lizziearobson@gmail.com>
2024-02-23 18:31:58 +0000

    racecar example

---
+++ stacks.js
@@ -0,0 +1,19 @@
+let letters = [];
+
+let word = "racecar";
+
+let rword = "";
+
+for (let i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+for (let i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
+
+if (rword === word) {
+  console.log(word + " is a palindrome.");
+} else {
+  console.log(word + " is not a palindrome.");
+}
